347e4b31d908f68daebe6873b1342d1b582099e2
cawlye <113624168+cawlye@users.noreply.github.com>
2022-11-09 16:25:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   gogoanime_url: "https://gogoanime.lu/", //gogoanime's website url
-  port: procces.env.PORT || 8000, //Port for website.
+  port: process.env.PORT || 8000, //Port for website.
 }
